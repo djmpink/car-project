ba54f40414774823f4848705650d07c967107c77
djmpink <343787567@qq.com>
2015-03-31 19:52:38 +0800

    test hello msg

--- car-web/src/main/java/com/pomelo/car/web/controller/HelloController.java
+++ car-web/src/main/java/com/pomelo/car/web/controller/HelloController.java
@@ -1,9 +1,16 @@
 package com.pomelo.car.web.controller;
 
-import net.sf.json.JSONObject;
+import com.pomelo.car.web.model.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -11,16 +18,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HelloController {
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String hello(){
+
+    // http://127.0.0.1:8081/hello.do?name=zl
+    @RequestMapping(value = "hello.do", method = RequestMethod.GET)
+    public void hello(HttpServletResponse response,String name) throws IOException {
         System.out.println("Hello spring MVC！");
-
-        //String jsonString = JSON.toJSONString(person, SerializerFeature.PrettyFormat);
-
-        JSONObject jsonObject =new JSONObject();
-        jsonObject.put("name", "zhoul");
-        jsonObject.put("age",26);
-
-        return "index";
+        Map map=new HashMap<String,String>();
+        map.put("name1","lizuoqing");
+        map.put("name2",name);
+        response.getWriter().println(map);
+        response.getWriter().close();
     }
+
+//    @RequestMapping(value = "hello.do", method = RequestMethod.GET)
+//    @ResponseBody
+//    public User hello(){
+//        System.out.println("Hello spring MVC！");
+//
+//        //String jsonString = JSON.toJSONString(person, SerializerFeature.PrettyFormat);
+//
+////        JSONObject jsonObject =new JSONObject();
+////        jsonObject.put("name", "zhoul");
+////        jsonObject.put("age",26);
+//
+//        User user =new User();
+//        user.setAge(20);
+//        user.setName("zl");
+//        return user;
+//    }
+
+
+
 }
